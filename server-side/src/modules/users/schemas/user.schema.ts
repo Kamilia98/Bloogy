59f1f68488f01b7211b4f5729319cd81d37ed694
Kamilia <90932389+Kamilia98@users.noreply.github.com>
2025-05-19 10:28:46 +0300

    Add required name property to User schema

--- server-side/src/modules/users/schemas/user.schema.ts
+++ server-side/src/modules/users/schemas/user.schema.ts
@@ -6,6 +6,9 @@ export type UserDocument = User & Document;
 
 @Schema()
 export class User {
+  @Prop({ required: true })
+  name: string;
+  
   @Prop({ required: true, unique: true })
   email: string;
 
