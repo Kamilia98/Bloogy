e89b56c262502c20adbab0ef1bdd8f4e551faba6
Kamilia <90932389+Kamilia98@users.noreply.github.com>
2025-05-19 21:02:48 +0300

    Implement Redux store and blogs slice: set up state management for blogs with async actions for fetching, adding, updating, and deleting blogs.

---
+++ client-side/src/store/features/blogs/blogsSlice.ts
@@ -0,0 +1,101 @@
+import {
+  createSlice,
+  createAsyncThunk,
+  type PayloadAction,
+} from '@reduxjs/toolkit';
+import axios from 'axios';
+import type { Blog } from '../../../models/BlogModel';
+
+interface BlogsState {
+  items: Blog[];
+  status: 'idle' | 'loading' | 'succeeded' | 'failed';
+  error: string | null;
+}
+
+const initialState: BlogsState = {
+  items: [],
+  status: 'idle',
+  error: null,
+};
+
+// Async thunks
+export const fetchBlogs = createAsyncThunk(
+  'blogs/fetchBlogs',
+
+  async (params: Record<string, any>) => {
+    const response = await axios.get('/api/blogs', { params });
+    return response.data;
+  },
+);
+
+export const addBlog = createAsyncThunk(
+  'blogs/addBlog',
+  async (blog: Omit<Blog, 'id' | 'createdAt' | 'updatedAt'>) => {
+    const response = await axios.post('/api/blogs', blog);
+    return response.data;
+  },
+);
+
+export const updateBlog = createAsyncThunk(
+  'blogs/updateBlog',
+  async (blog: Blog) => {
+    const response = await axios.put(`/api/blogs/${blog._id}`, blog);
+    return response.data;
+  },
+);
+
+export const deleteBlog = createAsyncThunk(
+  'blogs/deleteBlog',
+  async ({ id, token }: { id: string; token: string }) => {
+    console.log(id);
+    await axios.delete(`/api/blogs/${id}`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    return id;
+  },
+);
+
+const blogsSlice = createSlice({
+  name: 'blogs',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      // Fetch blogs
+      .addCase(fetchBlogs.pending, (state) => {
+        state.status = 'loading';
+      })
+
+      .addCase(fetchBlogs.fulfilled, (state, action: PayloadAction<Blog[]>) => {
+        state.status = 'succeeded';
+        state.items = action.payload;
+      })
+      .addCase(fetchBlogs.rejected, (state, action) => {
+        state.status = 'failed';
+        state.error = action.error.message || 'Failed to fetch blogs';
+      })
+
+      // Add blog
+      .addCase(addBlog.fulfilled, (state, action: PayloadAction<Blog>) => {
+        state.items.push(action.payload);
+      })
+
+      // Update blog
+      .addCase(updateBlog.fulfilled, (state, action: PayloadAction<Blog>) => {
+        const index = state.items.findIndex(
+          (blog) => blog._id === action.payload._id,
+        );
+        if (index !== -1) {
+          state.items[index] = action.payload;
+        }
+      })
+
+      .addCase(deleteBlog.fulfilled, (state, action: PayloadAction<string>) => {
+        state.items = state.items.filter((blog) => blog._id !== action.payload);
+      });
+  },
+});
+
+export default blogsSlice.reducer;
