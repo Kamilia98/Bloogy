3cd3040deeaf729ea38c0f322534e9a02f44a285
Kamilia <90932389+Kamilia98@users.noreply.github.com>
2025-05-19 10:35:36 +0300

    Refactor App component to wrap AuthProvider with ThemeProvider for improved context management.

--- client-side/src/App.tsx
+++ client-side/src/App.tsx
@@ -1,16 +1,17 @@
 import './App.css';
-import BlogForm from './components/BlogForm';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthProvider';
+import { ThemeProvider } from './contexts/ThemeProvider';
 import ProjectRoutes from './routes.tsx';
-
 function App() {
   return (
     <>
       <Router>
-        <AuthProvider>
-          <ProjectRoutes />
-        </AuthProvider>
+        <ThemeProvider>
+          <AuthProvider>
+            <ProjectRoutes />
+          </AuthProvider>
+        </ThemeProvider>
       </Router>
     </>
   );
