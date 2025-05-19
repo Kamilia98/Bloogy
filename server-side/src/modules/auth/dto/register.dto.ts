a6ffd62e6c394062a07eb48db75e9600297fca9c
Kamilia <90932389+Kamilia98@users.noreply.github.com>
2025-05-19 10:28:29 +0300

    Enhance RegisterDto validation by adding password complexity requirements

--- server-side/src/modules/auth/dto/register.dto.ts
+++ server-side/src/modules/auth/dto/register.dto.ts
@@ -1,4 +1,4 @@
-import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsString, Matches, MinLength } from 'class-validator';
 
 export class RegisterDto {
   @IsString()
@@ -9,5 +9,10 @@ export class RegisterDto {
   email: string;
   @IsString()
   @IsNotEmpty()
+  @MinLength(8)
+  @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/, {
+    message:
+      'Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, and one number.',
+  })
   password: string;
 }
