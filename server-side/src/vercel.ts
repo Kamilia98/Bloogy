9f41881c6ace3ef5e1dd6ae1dad403359dfc47ba
Kamilia <90932389+Kamilia98@users.noreply.github.com>
2025-05-26 22:19:58 +0300

    Update dependencies and restructure Vercel configuration for improved compatibility

---
+++ server-side/src/vercel.ts
@@ -0,0 +1,32 @@
+// src/vercel.ts
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as express from 'express';
+
+const expressApp = express();
+
+async function bootstrap() {
+  const app = await NestFactory.create(
+    AppModule,
+    new ExpressAdapter(expressApp),
+  );
+
+  const configService = app.get(ConfigService);
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  app.enableCors({
+    origin: configService.get<string>('CORS_ORIGIN'),
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
+  await app.init();
+}
+
+bootstrap();
+
+export default expressApp;
