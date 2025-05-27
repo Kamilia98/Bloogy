9a23ef6f4705f2fd19e7ce935322e311004605ce
Kamilia <90932389+Kamilia98@users.noreply.github.com>
2025-05-27 17:54:43 +0300

    downgrade tailwind

--- client-side/vite.config.ts
+++ client-side/vite.config.ts
@@ -1,13 +1,12 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
   console.log('VITE_API_BASE_URL:', env.VITE_API_BASE_URL);
 
   return {
-    plugins: [react(), tailwindcss()],
+    plugins: [react()],
     server: {
       port: 3000,
       proxy: {
