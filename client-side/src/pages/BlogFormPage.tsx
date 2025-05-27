5bf2836a469efee7525ab1b13e0443b5ad1f919d
Kamilia <90932389+Kamilia98@users.noreply.github.com>
2025-05-27 22:02:57 +0300

    Refactor AddBlogPage component to adjust padding for improved layout responsiveness

--- client-side/src/pages/BlogFormPage.tsx
+++ client-side/src/pages/BlogFormPage.tsx
@@ -2,7 +2,7 @@ import BlogForm from '../components/BlogForm';
 
 export default function AddBlogPage() {
   return (
-    <div className="w-full p-12">
+    <div className="w-full p-4 md:p-8 lg:p-12">
       <BlogForm />
     </div>
   );
