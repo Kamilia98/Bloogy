9fc0cf59454ca9490181085d4f39db4fc77f837a
Kamilia <90932389+Kamilia98@users.noreply.github.com>
2025-05-27 21:50:35 +0300

    Refactor BackButton component to use Link instead of button for improved navigation and consistency

--- client-side/src/components/common/BackButton.tsx
+++ client-side/src/components/common/BackButton.tsx
@@ -1,15 +1,14 @@
 import { ArrowLeft } from 'lucide-react';
-import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 export default function BackButton() {
-  const navigate = useNavigate();
   return (
-    <button
-      onClick={() => navigate('/blogs')}
-      className="hover:text-primary flex items-center gap-2 text-gray-600 transition-colors"
+    <Link
+      to={'/blogs'}
+      className="flex items-center gap-2 text-gray-600 transition-colors hover:text-primary"
     >
       <ArrowLeft size={18} />
       Back to blogs
-    </button>
+    </Link>
   );
 }
