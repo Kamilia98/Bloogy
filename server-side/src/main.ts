bfabbccb9fa592e155ff6e2c2e84d61e1dd7b50c
Kamilia <90932389+Kamilia98@users.noreply.github.com>
2025-05-29 19:45:26 +0300

    Refactor cookie-parser import to use default import syntax; update vercel.json to correct source file paths

--- server-side/src/main.ts
+++ server-side/src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import cookieParser from 'cookie-parser';
+import * as cookieParser from 'cookie-parser';
 
 export default async function bootstrap() {
   console.log('[main] Starting NestJS application...');
